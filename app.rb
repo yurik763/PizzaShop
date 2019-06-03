df8b116e9d4127fd07cf9fba2eae17b23a1cb44d
yurik763 <yurik763@tut.by>
2019-06-03 23:59:18 +0300

    create Rakefile, and require activerecotd

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :databese, "sqlite3:pizzashop.db"
+
+class Product < ActiveRecord::Base
+end
 
 get '/' do
 	erb :index
