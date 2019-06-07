2c913e34f7ee2a38a784b70cf21e28a6c0700aa5
yurik763 <yurik763@tut.by>
2019-06-07 22:57:25 +0300

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -20,6 +20,9 @@ function add_to_cart(id)
 	alert('Количество пицц в корзине:' + cart_get_number_of_items());
 }
 
+ 
+
+
 function cart_get_number_of_items()
 {
 	var cnt = 0;
@@ -34,4 +37,20 @@ function cart_get_number_of_items()
 		}
 	}
 	return cnt;
+}
+
+function cart_get_orders()
+{
+	var orders = '';
+	for(var i = 0; i < window.localStorage.length; i++)  // вместо each
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в руби: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ','
+		}
+	}
+	return orders;
 }
